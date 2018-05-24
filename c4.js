93121b592fb779e1d2b5780c22106dc7e70c1382
Kevin La Casse <kl.forthwind@gmail.com>
2018-05-24 11:47:56 -0500

    Make game check horizontal, vertical, and if board is full

--- c4.js
+++ c4.js
@@ -1,12 +1,15 @@
 let firstPlayer;
 let board;
 let ai;
-let winConditions;
+let wc;
+let winner;
+let noWinner = true;
 
 function setup() {
   board = new Board();
   ai = new AI();
-  winConditions = new WinConditions();
+  wc = new WinConditions();
+  winner = null;
 
   firstPlayer = Math.floor(Math.random()*2);
 }
@@ -17,14 +20,30 @@ function draw() {
 
 function play() {
   if (firstPlayer % 2 === 0) {
-    let didMove = ai.playMove(board, firstPlayer);
-    if (didMove) firstPlayer++;
+    winner = wc.getWinner(board);
+    if (winner !== null) {
+      if (noWinner) {
+        noWinner = false;
+        console.log(winner);
+      }
+    } else {
+      let didMove = ai.playMove(board, firstPlayer);
+      if (didMove) firstPlayer++;
+    }
   }
 }
 
 function mousePressed() {
-  if (firstPlayer % 2 === 1) {
-    let didMove = board.putPiece(firstPlayer, board.getCol(mouseX));
-    if (didMove) firstPlayer++;
+  if (firstPlayer % 2 === 1 && winner == null) {
+    winner = wc.getWinner(board);
+    if (winner !== null) {
+      if (noWinner) {
+        noWinner = false;
+        console.log(winner);
+      }
+    } else {
+      let didMove = board.putPiece(firstPlayer, board.getCol(mouseX));
+      if (didMove) firstPlayer++;
+    }
   }
 }
--- winconditions.js
+++ winconditions.js
@@ -1,30 +1,107 @@
 class WinConditions {
 
   constructor() {
-    this.winner = null;
   }
 
-  getWinner(grid) {
-    this.winner = null;
-    checkHorizontal(grid);
-    checkVertical(grid);
-    checkDiagonalDown(grid);
-    checkDiagonalUp(grid);
+  getWinner(board) {
+    let hor = this.checkHorizontal(board);
+    if (hor !== null) return hor;
+    let vert = this.checkVertical(board);
+    if (vert !== null) return vert;
+    let diagDown = this.checkDiagonalDown(board);
+    if (diagDown !== null) return diagDown;
+    let diagUp = this.checkDiagonalUp(board);
+    if (diagUp !== null) return diagUp;
+    let gridFull = this.checkGridSpace(board);
+    if (gridFull !== null) return gridFull;
+    return null;
   }
 
-  checkHorizontal(grid) {
-
+  checkHorizontal(board) {
+    let winner = null;
+    for (let i = 0; i < board.rows; i++) {
+      let redTiles = [];
+      let yellowTiles = [];
+      for (let j = 0; j < board.cols; j++) {
+        if (board.grid[i][j] === board.red) {
+          redTiles[redTiles.length] = j;
+        } else if (board.grid[i][j] === board.yellow) {
+          yellowTiles[yellowTiles.length] = j;
+        }
+      }
+      let fourYellow = this.fourConsecutiveNumbers(yellowTiles);
+      let fourRed = this.fourConsecutiveNumbers(redTiles);
+      if (fourYellow) {
+        winner = "Yellow";
+      } else if (fourRed) {
+        winner = "Red";
+      }
+      if (winner !== null) break;
+    }
+    return winner;
   }
 
-  checkVertical(grid) {
-
+  checkVertical(board) {
+    let winner = null;
+    for (let i = 0; i < board.cols; i++) {
+      let redTiles = [];
+      let yellowTiles = [];
+      for (let j = 0; j < board.rows; j++) {
+        if (board.grid[j][i] === board.red) {
+          redTiles[redTiles.length] = j;
+        } else if (board.grid[j][i] === board.yellow) {
+          yellowTiles[yellowTiles.length] = j;
+        }
+      }
+      let fourYellow = this.fourConsecutiveNumbers(yellowTiles);
+      let fourRed = this.fourConsecutiveNumbers(redTiles);
+      if (fourYellow) {
+        winner = "Yellow";
+      } else if (fourRed) {
+        winner = "Red";
+      }
+      if (winner !== null) break;
+    }
+    return winner;
   }
 
-  checkDiagonalDown(grid) {
-
+  checkDiagonalDown(board) {
+    return null;
   }
 
-  checkDiagonalUp(grid) {
+  checkDiagonalUp(board) {
+    return null;
+  }
 
+  checkGridSpace(board) {
+    let spaceFull = true;
+    for (let i = 0; i < board.rows; i++) {
+      for (let j = 0; j < board.cols; j++) {
+        if (board.grid[i][j] === board.black) {
+          spaceFull = false;
+          break;
+        }
+      }
+    }
+    return (spaceFull) ? "tie" : null;
+  }
+
+  fourConsecutiveNumbers(arr) {
+    if (arr.length >= 4) {
+      let num = arr[0];
+      let count = 1;
+      for (let i = 1; i <arr.length; i++) {
+        if (arr[i] !== num + count) {
+          num = arr[i];
+          count = 1;
+        } else if (arr[i] === num + count) {
+          count++;
+        }
+        if (count >= 4) break;
+      }
+      return count >= 4;
+    } else {
+      return false;
+    }
   }
 }
